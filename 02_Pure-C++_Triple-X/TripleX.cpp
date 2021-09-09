b41b9cdd410d19f0073492dd0842e82805e11112
Skywalker <70905670+skywalker1319@users.noreply.github.com>
2021-09-10 00:59:17 +0530

    Initial Commit

---
+++ 02_Pure-C++_Triple-X/TripleX.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    /*
+    Developer: Master Skywalker
+    Purpose: Game Development Project with C++
+    Stardate: 12021.253.00:49:00
+    */
+
+   std::cout <<"ASAI's research lab is under attack, break the codes to get into the datacentre facility, cut the hard line & save the research data from going into wrong hands...";
+   std::cout << std::endl;
+   std::cout <<"Now, Enter The Code:\t";
+   
+
+    return 0;
+}
