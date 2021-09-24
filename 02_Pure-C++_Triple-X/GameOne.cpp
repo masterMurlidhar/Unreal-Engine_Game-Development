3fa70b4a0ee4c1984e3f02ceb721b1d514d193a5
Skywalker <70905670+skywalker1319@users.noreply.github.com>
2021-09-24 23:59:11 +0530

    GameOne

--- 02_Pure-C++_Triple-X/GameOne.cpp
+++ 02_Pure-C++_Triple-X/GameOne.cpp
@@ -5,32 +5,33 @@
 */
 
 #include <iostream>
+#include <ctime>
 
 
 // User Defined Functions
 void GameIntro(int Difficulty)
 {
-    std::cout << "\n\t\t\"ASAI's Research Lab Is Under Attack, Break The Codes To Get Into The 'Level "<< Difficulty; 
-    std::cout << "' Secured Datacentre Facility, Cut The Hard Line & Save The Research Data From Going Into The Wrong Hands...\"\n\n\n";
+    std::cout << "\n\t AIM : \n\t***  ASAI's Research Lab Is Under Attack, Break The Codes To Get Into The 'LEVEL "<< Difficulty; 
+    std::cout << "' Secured Datacentre Facility, Cut The Hard Line & Save The Research Data From Going Into The Wrong Hands...  *** \n\n\n";
 
 }
 
 
-bool PlayGame()     // bool must return boolean...
+bool PlayGame(int Difficulty)     // bool must return boolean...
 {
-    GameIntro(9);
+    GameIntro(Difficulty);
 
-    int CodeA = 3;
-    int CodeB = 6;
-    int CodeC = 9;
+    int CodeA = rand() % Difficulty + Difficulty;    //  rand() genrates values upto 32000. To get smaller values, we're using modulo operator...
+    int CodeB = rand() % Difficulty + Difficulty;    //  "Difficulty + Difficulty" to make it much difficult...
+    int CodeC = rand() % Difficulty + Difficulty;    //  " +1 " to remove any 0 values & make it more difficult...
 
     int CodeSum = CodeA + CodeB + CodeC;
     int CodeProduct = CodeA * CodeB * CodeC;
 
     std::cout << ">_    Here's Some Informations About The Code...\n\n";
-    std::cout << "+ The Total Numbers Of Code: 3\n";
-    std::cout << "+ The Codes Join Upto: " << CodeSum << std::endl;
-    std::cout << "+ The Codes Product Upto: " << CodeProduct << std::endl;
+    std::cout << "+ The Total Series Of Code:   3  ( X X X )\n";
+    std::cout << "+ The Codes Join Upto:        " << CodeSum << std::endl;
+    std::cout << "+ The Codes Product Upto:     " << CodeProduct << std::endl;
 
     int GuessA, GuessB, GuessC;
     std::cout << "\n\tNow, Enter The Codes: ";
@@ -41,12 +42,14 @@ bool PlayGame()     // bool must return boolean...
 
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "\n\t\t\t Welcome To The ASAI'S Datacentre Facility :)\n\n";
+        std::cout << "\n\t\t\t ###   Welcome To The ASAI'S 'LEVEL "<< Difficulty;
+        std::cout << "' Datacentre Facility :)   ### \n\n";
         return true;
     }   
     else
     {
-        std::cout << "\n\t\t Alert! There's A Breach In The Datacentre Facility :(\n\n";
+        std::cout << "\n\t\t ###   Alert! There's A Breach In The 'LEVEL "<< Difficulty;
+        std::cout << "' Datacentre Facility :(   ### \n\n";
         // std::cout << "\tWell, The Codes Were: " << CodeA << CodeB << CodeC << std::endl;
         return false;
     }
@@ -57,23 +60,30 @@ bool PlayGame()     // bool must return boolean...
 // Main Function
 int main(int argc, char const *argv[])
 {   
+    srand(time(NULL));     // More complex random numbers based on time of the day...
+
     int LevelDifficulty = 1;    // Remember The Variable Scope...
+    const int MaxDifficulty = 5;
 
     // While Loop
-    while (true)
+    while (LevelDifficulty <= MaxDifficulty)
     {   
-        bool bLevelComplete = PlayGame();
+        // std::cout << rand() % 5 << '\n';
+        bool bLevelComplete = PlayGame(LevelDifficulty);
         std::cin.clear();   // Clears any error...
         std::cin.ignore();  // Discard the buffer...
-        
+
         if (bLevelComplete)
         {
             ++LevelDifficulty;    // LevelDefficulty = LevelDifficulty + 1
         }
-        
+        else
+        {
+            std::cout << "\n ###   You've Entered An Incorrect Code :(  After One More Unsuccessful Attempt, Lockdown Procedures Will Be Initiated...  ### \n\n";
+        }
         
     }
-
+    std::cout << "\n ###   You've Saved The ASAI's Research Lab Today, You're Awesome :)   ### \n\n";
     return 0;
 }
 
